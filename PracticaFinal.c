b07bcefca79cb1113a0bdba7efec962a27cd459b
adrianetete <adricalrojo@gmail.com>
2016-11-30 19:46:47 +0100

    He hecho el main

--- PracticaFinal.c
+++ PracticaFinal.c
@@ -1,2 +1,16 @@
 
 #include <stdio.h>
+#include <string.h>
+
+int main ( int argc, const char* argv[] ) {
+
+   if (argc != 4){
+      
+      printf("Error en el numero de argumentos\n");
+      exit(-1);
+   }
+
+   
+   exit(0);
+}
+
