bfb04d376fa4a32c643b9d1353d182a871a2850a
Juanprinfits <cuentademistrabajos@gmail.com>
2016-12-03 16:35:29 +0100

    hasta el metodo de primos

--- PracticaFinal.c
+++ PracticaFinal.c
@@ -1,16 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<string.h>
 
-#include <stdio.h>
-#include <string.h>
+//Declaracion de variables globales
+int **bufferini;
+int bufferfin;
 
-int main ( int argc, const char* argv[] ) {
+int i=0,j=0,consum=0;
 
-   if (argc != 4){
-      
-      printf("Error en el numero de argumentos\n");
-      exit(-1);
-   }
+//Semaforos que indican si esta vacio o hay dato en los buffers
+sem_t vacioB1;
+sem_t datoB1;
+sem_t vacioB2;
+sem_t datoB2;
 
-   
-   exit(0);
+//Semaforos para garantizar la exclusion mutua en las variables globales
+
+sem_t mutexI;
+sem_t mutexJ;
+sem_t mutexConsum;
+
+//Metodo comprobacion de que sea primo
+int esPrimo (int num) 
+{
+int num,i,a;	
+for(i=1;i<=num;i++)
+	{
+		if(num==0)
+		a++;
+	}
+	
+	if(a==2);
+	{
+	 //es primo
+	}
+	else
+	{
+	//no es primo
+	}
 }
 
+void *productor(void *arg){
+	int numAleat = rand() % 100000;
+	
