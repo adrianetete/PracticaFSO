b872df38b5f9f6d1673dba5cee6ca8fa91a69f70
Juanprinfits <cuentademistrabajos@gmail.com>
2016-12-03 20:10:43 +0100

    metodo es primo y productor

--- PracticaFinal.c
+++ PracticaFinal.c
@@ -7,13 +7,14 @@
 //Declaracion de variables globales
 int **bufferini;
 int bufferfin;
+int nNumeros;
 
 int i=0,j=0,consum=0;
 
 //Semaforos que indican si esta vacio o hay dato en los buffers
-sem_t vacioB1;
+sem_t espacioB1;
 sem_t datoB1;
-sem_t vacioB2;
+sem_t espacioB2;
 sem_t datoB2;
 
 //Semaforos para garantizar la exclusion mutua en las variables globales
@@ -23,25 +24,36 @@ sem_t mutexJ;
 sem_t mutexConsum;
 
 //Metodo comprobacion de que sea primo
-int esPrimo (int num) 
-{
-int num,i,a;	
-for(i=1;i<=num;i++)
+int esPrimo (int num){
+	int num,i,a;
+	int esPrimo=1;	
+	for(i=0;i<num;i++)
 	{
-		if(num==0)
-		a++;
+		if(num%i==0)
+		a++;		
 	}
-	
 	if(a==2);
-	{
-	 //es primo
-	}
-	else
-	{
-	//no es primo
-	}
+	return esPrimo;
+	
 }
 
-void *productor(void *arg){
-	int numAleat = rand() % 100000;
+void productor(int nNum){
+	int i;
+	for(i=0;i<nNum;i++){
+		int numAleat = rand();
+		sem_wait(&espacioB1);							
+		buffer1[i % tam_buffer] = numAleat;
+		sem_post(&datoB1);
+	}
+		
+		
+
+
+
+
+
+
+
+
+
 	
